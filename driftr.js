32ad6f8f5aae91f1cdd1a3d5b642d00e3d6070ae
Watal M. Iwasaki <heavy.watal@gmail.com>
2014-09-30 02:43:36 +0900

    wrote functions: bernoulli and binomial

--- driftr.js
+++ driftr.js
@@ -1,3 +1,18 @@
 document.write("Hello, Javascript.<br />");
-var date = new Date();
-document.write(date.toLocaleString());
+
+(function() {
+    function bernoulli(prob) {
+        return Math.random() < prob;
+    }
+
+    function binomial(size, prob) {
+        var cnt = 0;
+        for (var i=0; i<size; ++i) {
+            if (bernoulli(prob)) {++cnt;}
+        }
+        return cnt;
+    }
+
+    var success = binomial(100, 0.3);
+    console.log(success);
+})()
