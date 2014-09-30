cbe91a9ed613db2f85adc254c366c8be0b2c7735
Watal M. Iwasaki <heavy.watal@gmail.com>
2014-09-30 13:30:49 +0900

    implemented drift and selection

--- driftr.js
+++ driftr.js
@@ -1,18 +1,29 @@
 document.write("Hello, Javascript.<br />");
 
 (function() {
-    function bernoulli(prob) {
+
+    function random_bernoulli(prob) {
         return Math.random() < prob;
     }
 
-    function binomial(size, prob) {
+    function random_binomial(size, prob) {
         var cnt = 0;
         for (var i=0; i<size; ++i) {
-            if (bernoulli(prob)) {++cnt;}
+            if (random_bernoulli(prob)) {++cnt;}
         }
         return cnt;
     }
 
-    var success = binomial(100, 0.3);
-    console.log(success);
+    function evolve(N, p, s, T) {
+        freq = [p];
+        for (var t=0; t<T; ++t) {
+            p = random_binomial(N, (1 + s) * p) / N;
+            freq.push(p)
+        }
+        return freq;
+    }
+
+    console.log(random_binomial(100, 0.3));
+    console.log(evolve(100, 0.5, 0.01, 100));
+
 })()
