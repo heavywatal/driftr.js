4c2c4c6709c4ee21a399f7e23498437a01aa7252
Watal M. Iwasaki <heavy.watal@gmail.com>
2014-12-17 19:41:10 +0900

    Close #3: Refresh svg after window resize

--- driftr.js
+++ driftr.js
@@ -135,14 +135,12 @@
 
     var svg_padding = {
         top:    20,
-        right:  100,
+        right:  20,
         bottom: 60,
         left:   80
     };
 
-    var svg = d3.select("#graph")
-            .append("svg")
-            .attr("height", 400);
+    var svg = d3.select("#graph").append("svg");
 
     var fixation_divs = d3.select("#graph")
             .append("div")
@@ -163,7 +161,7 @@
             .attr("transform",
                   "translate("+svg_padding.left+","+svg_padding.top+")")
             .attr("height",
-                  svg.attr("height") - svg_padding.top - svg_padding.bottom);
+                  parseInt(svg.style("height")) - svg_padding.top - svg_padding.bottom);
     var panel_bg = panel.append("rect")
             .attr("class", "panel_background")
             .attr("height", panel.attr("height"));
@@ -191,32 +189,38 @@
             .attr("text-anchor", 'middle');
 
     function update_width() {
-        var col_width = 40;
-        var width = parseInt(d3.select("body").style("width"));
-        svg.attr("width", width - col_width);
-        scale_x.range([0, width - svg_padding.left - svg_padding.right - col_width]);
-    }
-
-    function draw(only_axis) {
+        var width = parseInt(d3.select("#graph").style("width"));
+        var fixation_width = parseInt(d3.select("svg").style("padding-right"));
+        svg.attr("width", width - fixation_width);
         var svg_width = parseInt(svg.attr("width"));
         var panel_width = svg_width - svg_padding.left - svg_padding.right;
         var panel_height = parseInt(panel.attr("height"));
-        var N = parseFloat(params_now["popsize"]);
-        var s = parseFloat(params_now["selection"]);
-        var q = parseFloat(params_now["frequency"]);
-        var T = parseInt(params_now["observation"]);
-        var rep = parseInt(params_now["replicates"]);
-        panel.selectAll("path").remove();
-        panel.selectAll("g").remove();
-
         panel_bg.attr("width", panel_width);
-        scale_x.domain([0, T]);
+        scale_x.range([0, panel_width]);
+        d3.select("#xaxis")
+            .attr("transform",
+                  "translate(0," + panel_height + ")")
+            .call(x_axis);
+        x_axis_label.attr("transform", "translate("+
+              ((svg_width - svg_padding.left - svg_padding.right) / 2)
+                          +","+ (panel_height + 50) +")");
+        panel.selectAll("path").remove();
+        plot();
+    }
+
+    function init_svg() {
+        update_width();
+        var svg_width = parseInt(svg.attr("width"));
+        var panel_width = svg_width - svg_padding.left - svg_padding.right;
+        var panel_height = parseInt(panel.attr("height"));
 
         panel.append("g")
+            .attr("id", "xaxis")
             .attr("transform",
                   "translate(0," + panel.attr("height") + ")")
             .call(x_axis);
         panel.append("g")
+            .attr("id", "yaxis")
             .call(y_axis);
 
         x_axis_label.attr("transform", "translate("+
@@ -224,23 +228,52 @@
               +","+ (panel_height + 50) +")");
         y_axis_label.attr("transform",
               "translate(-50,"+ panel_height/2 +")rotate(-90)");
+    }
 
-        d3.selectAll("#fixation label.value").text(0);
-
-        if (only_axis) {rep=0;}
+    function simulation() {
+        var N = parseFloat(params_now["popsize"]);
+        var s = parseFloat(params_now["selection"]);
+        var q0 = parseFloat(params_now["frequency"]);
+        var T = parseInt(params_now["observation"]);
+        var rep = parseInt(params_now["replicates"]);
+        scale_x.domain([0, T]);
         for (var i=0; i<rep; ++i) {
-            var path = panel.append("path");
-            var freq = [q];
+            var qt = q0;
+            var trajectory = [q0];
             var repl_delay = rep * T / 5 + 600 * i / rep;
-            for (var t=0; t<T; ++t) {
-                var qt = freq[t];
-                freq.push(random_binomial(N, (1 + s) * qt / (1 + s * qt)) / N);
-                path.transition().delay(repl_delay + 23 * t).ease("linear")
-                    .attr("d", line(freq));
+            for (var t=1; t<=T; ++t) {
+                qt = random_binomial(N, (1 + s) * qt / (1 + s * qt)) / N;
+                trajectory.push(qt);
             }
-            if (freq.slice(-1)[0] == 1) {
+            results.push(trajectory);
+        }
+        return results;
+    }
+
+    function plot() {
+        var rep = results.length;
+        for (var i=0; i<rep; ++i) {
+            var trajectory = results[i];
+            panel.append("path").attr("d", line(trajectory));
+        }
+    }
+
+    function animation() {
+        var rep = results.length;
+        for (var i=0; i<rep; ++i) {
+            var trajectory = results[i];
+            var T = trajectory.length;
+            var repl_delay = rep * T / 5 + 600 * i / rep;
+            var path = panel.append("path");
+            for (var t=0; t<=T; ++t) {
+                var part = trajectory.slice(0, t);
+                path.transition().delay(repl_delay + 23 * t).ease("linear")
+                    .attr("d", line(part));
+            }
+            var qT = trajectory.slice(-1)[0];
+            if (qT == 1) {
                 fixation_increment("#fixed");
-            } else if (freq.slice(-1)[0] == 0) {
+            } else if (qT == 0) {
                 fixation_increment("#lost");
             } else {
                 fixation_increment("#polymorphic");
@@ -254,26 +287,29 @@
     }
 
     var footer = d3.select("#footer");
-
     footer.append("a")
         .attr("class", "button")
         .attr("href", "https://github.com/heavywatal/driftr.js/releases/latest")
         .text(t("footer.download"));
-
     footer.append("a")
         .attr("class", "button")
         .attr("href", "https://github.com/heavywatal/driftr.js/issues")
         .text(t("footer.report"));
-
     footer.append("a")
         .attr("class", "button")
         .attr("href", "https://github.com/heavywatal/driftr.js")
         .text(t("footer.develop"));
 
-    update_width();
-    draw(true);
+    var results = [];
+    init_svg();
 
     d3.select(window).on("resize", update_width);
-    d3.select("#start").on("click", draw);
+    d3.select("#start").on("click", function(){
+        panel.selectAll("path").remove();
+        d3.selectAll("#fixation label.value").text(0);
+        results = [];
+        simulation();
+        animation();
+    });
 
 })(d3, x18n, t);
