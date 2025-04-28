fc8d3197c3659d50d9bf13410d9b6a7d424e2e6a
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2025-04-28 13:57:40 +0900

    :art: Use d3-random directly

--- src/genetics.js
+++ src/genetics.js
@@ -1,12 +1,12 @@
 'use strict'
-import * as random from './random.js'
+import { randomBernoulli, randomBinomial } from 'd3-random'
 
 export function wrightFisher (N, s, q0, T, qPrime) {
   let qt = q0
   const trajectory = [[0, q0]]
   const step = Math.max(T / 1000, 1)
   for (let t = 1; t <= T; ++t) {
-    qt = random.binomial(N, qPrime(qt, s)) / N
+    qt = randomBinomial(N, qPrime(qt, s))() / N
     if (t % step === 0) {
       trajectory.push([t, qt])
     }
@@ -48,10 +48,10 @@ export function moranHaploid (N, s, q0, T) {
   const step = Math.max(T / 1000, 1)
   for (let t = 1; t <= T * N; ++t) {
     const pMutRep = s1 * Nq / (s1 * Nq + (N - Nq))
-    if (random.bernoulli(Nq / N)) { // a mutant dies
-      if (!random.bernoulli(pMutRep)) { --Nq }
+    if (randomBernoulli(Nq / N)()) { // a mutant dies
+      if (!randomBernoulli(pMutRep)()) { --Nq }
     } else { // a wildtype dies
-      if (random.bernoulli(pMutRep)) { ++Nq }
+      if (randomBernoulli(pMutRep)()) { ++Nq }
     }
     if (t % (step * N) === 0) {
       trajectory.push([t / N, Nq / N])
